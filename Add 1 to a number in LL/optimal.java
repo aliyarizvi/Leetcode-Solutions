946deacfbac49ce2c150e7a5f441be4ce08a62bd
Aliya Rizvi <130211237+aliyarizvi@users.noreply.github.com>
2024-06-19 23:59:09 +0530

    Create optimal.java

---
+++ Add 1 to a number in LL/optimal.java
@@ -0,0 +1,26 @@
+class Solution
+{
+    public static int helper(Node temp){
+        if(temp==null)
+        return 1;
+    
+    int carry=helper(temp.next);
+    temp.data=temp.data+carry;
+      
+    if(temp.data<10)
+    return 0;
+      
+    temp.data=0;
+    return 1;
+}
+    public static Node addOne(Node head) 
+    { 
+        int carry = helper(head);
+        if(carry==1){
+            Node newNode=new Node(1);
+            newNode.next=head;
+            head=newNode;
+        }
+        return head;
+    }
+}
