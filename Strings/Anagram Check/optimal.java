106d843909f58534821206823f255e214dec6d87
Aliya Rizvi <130211237+aliyarizvi@users.noreply.github.com>
2024-07-16 03:04:45 +0530

    Create optimal.java

---
+++ Strings/Anagram Check/optimal.java
@@ -0,0 +1,19 @@
+//Time complexity: O(n)
+//Space COmplexity: O(1)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr=new int[26];
+
+        for(char i:s.toCharArray()){
+            arr[i-'a']++;
+        }
+        for(char i:t.toCharArray()){
+            arr[i-'a']--;
+        }
+        for(int i:arr){
+            if(i!=0)
+            return false;
+        }
+        return true;
+    }
+}
